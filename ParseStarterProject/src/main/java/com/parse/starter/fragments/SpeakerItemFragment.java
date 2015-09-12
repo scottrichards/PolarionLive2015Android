589859e9774e2fd21347526b325439d7402deaff
Scott Richards <scott.srichards@gmail.com>
2015-09-12 13:56:32 -0700

    Speakers Working. Code Cleanup, Fix up Agenda View

--- ParseStarterProject/src/main/java/com/parse/starter/fragments/SpeakerItemFragment.java
+++ ParseStarterProject/src/main/java/com/parse/starter/fragments/SpeakerItemFragment.java
@@ -17,7 +17,6 @@ import com.parse.ParseQuery;
 import com.parse.starter.R;
 import com.parse.starter.adapters.AgendaAdapter;
 import com.parse.starter.adapters.SpeakersAdapter;
-import com.parse.starter.fragments.dummy.DummyContent;
 import com.parse.starter.model.Speaker;
 
 import java.util.ArrayList;
@@ -81,13 +80,6 @@ public class SpeakerItemFragment extends ListFragment {
         ParseObject.registerSubclass(Speaker.class);
 
         mSpeakersAdapter = new SpeakersAdapter(getActivity(), new ArrayList<Speaker>());
-        // TODO: Change Adapter to display your content
-//        setListAdapter(new ArrayAdapter<DummyContent.DummyItem>(getActivity(),
-//                android.R.layout.simple_list_item_1, android.R.id.text1, DummyContent.ITEMS));
-
-
-//        setListAdapter(new ArrayAdapter<Speaker>(getActivity(),
-//                        android.R.layout.simple_list_item_1, android.R.id.text1, mSpeakersAdapter.mSpeakers));
         updateData();
     }
 
@@ -149,7 +141,7 @@ public class SpeakerItemFragment extends ListFragment {
         if (null != mListener) {
             // Notify the active callbacks interface (the activity, if the
             // fragment is attached to one) that an item has been selected.
-            mListener.onSelectSpeakerItem(DummyContent.ITEMS.get(position).id);
+ //           mListener.onSelectSpeakerItem(DummyContent.ITEMS.get(position).id);
         }
     }
 
@@ -164,7 +156,6 @@ public class SpeakerItemFragment extends ListFragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         public void onSelectSpeakerItem(String id);
     }
 
