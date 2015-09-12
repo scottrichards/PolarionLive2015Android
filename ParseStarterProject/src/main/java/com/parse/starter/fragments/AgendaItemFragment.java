9d13bbc999e9b88445e860b5aa6cd848431a84e0
Scott Richards <scott.srichards@gmail.com>
2015-09-12 14:58:08 -0700

    Fix up Agenda on Samsung Tab

--- ParseStarterProject/src/main/java/com/parse/starter/fragments/AgendaItemFragment.java
+++ ParseStarterProject/src/main/java/com/parse/starter/fragments/AgendaItemFragment.java
@@ -107,7 +107,7 @@ public class AgendaItemFragment extends ListFragment implements AbsListView.OnIt
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_agendaitem, container, false);
+        View view = inflater.inflate(R.layout.fragment_agenda, container, false);
 
         // Set the adapter
         mListView = (AbsListView) view.findViewById(android.R.id.list);
