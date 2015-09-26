2c8cea2f30cd970a00bfdb5fd34c683a745e7681
Scott Richards <Scott.Richards@babycenter.com>
2015-09-26 12:07:46 -0700

    Got Social Working.

--- ParseStarterProject/src/main/java/com/parse/starter/fragments/SocialFragment.java
+++ ParseStarterProject/src/main/java/com/parse/starter/fragments/SocialFragment.java
@@ -1,13 +1,20 @@
 package com.parse.starter.fragments;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.parse.LogOutCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 import com.parse.starter.R;
 
 /**
@@ -19,32 +26,21 @@ import com.parse.starter.R;
  * create an instance of this fragment.
  */
 public class SocialFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+    private View mView;
 
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment SocialFragment.
      */
     // TODO: Rename and change types and number of parameters
     public static SocialFragment newInstance(String param1, String param2) {
         SocialFragment fragment = new SocialFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+
         return fragment;
     }
 
@@ -55,17 +51,33 @@ public class SocialFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_social, container, false);
+        mView =  inflater.inflate(R.layout.fragment_social, container, false);
+        Button postButton = (Button)mView.findViewById(R.id.postButton);
+        postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editText = (EditText)mView.findViewById(R.id.editText);
+                String userEntry = editText.getText().toString();
+
+                Intent textShareIntent = new Intent(Intent.ACTION_SEND);
+                textShareIntent.putExtra(Intent.EXTRA_TEXT, userEntry);
+                textShareIntent.setType("text/plain");
+
+                //startActivity(textShareIntent);
+                // ^^ this auto-picks the defined default program for a content type, but since we want users to
+                //    have options, we instead use the OS to create a chooser for users to pick from
+
+                startActivity(Intent.createChooser(textShareIntent, "Share text with..."));
+            }
+        });
+        return mView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
